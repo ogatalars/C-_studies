d478090c5e0333fe62a76cbbd34bc12fb4319236
ogatalars <ogataogara@gmail.com>
2023-04-13 18:12:03 -0300

    Add: MathMax method

---
+++ CMath.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(5, 10));
+        }
+    }
+}
