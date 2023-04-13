90bb0313209ca1d269f0508e225b2f2fbcf6a697
ogatalars <ogataogara@gmail.com>
2023-04-13 18:09:32 -0300

    Add: Comparator or and &&

---
+++ Comparators.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            Console.WriteLine(x > y); // returns True because 5 is greater than 3
+            Console.WriteLine(y < x); // 
+        }
+    }
+}
