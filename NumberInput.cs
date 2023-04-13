240214087c79bc8f8aed011f7e762fe48fba8d5f
ogatalars <ogataogara@gmail.com>
2023-04-13 16:07:41 -0300

    Add: user input with num

---
+++ NumberInput.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace UserInputExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int myNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine = ("Number is: " + userName);
+        }
+    }
+}
