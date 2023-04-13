f529a49e70e9c3513aa94faac7d3fdfbda884ee6
ogatalars <ogataogara@gmail.com>
2023-04-12 21:00:07 -0300

    Add: Input Example

---
+++ UserInput.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace UserInputExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a username: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine = ("UserName is: " + userName);
+        }
+    }
+}
