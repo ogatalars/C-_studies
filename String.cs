e88605592db89d7756988662582408a2b46321d6
ogatalars <ogataogara@gmail.com>
2023-04-13 21:08:26 -0300

    Add: string length idea

---
+++ String.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+        }
+    }
+}
